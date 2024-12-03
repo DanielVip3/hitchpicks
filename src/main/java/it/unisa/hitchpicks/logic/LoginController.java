69988ad300af291e13495346633ce71be2ae2889
RicoDerous <fede2003710@gmail.com>
2024-12-03 21:44:16 +0100

    ♻️ Fixed LoginController style

--- src/main/java/it/unisa/hitchpicks/logic/LoginController.java
+++ src/main/java/it/unisa/hitchpicks/logic/LoginController.java
@@ -6,7 +6,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
   @GetMapping("/login")
-  public String login() {
-        return "login";
-    }
+  public String login() { return "login"; }
 }
